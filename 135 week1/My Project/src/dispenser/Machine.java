a2581015f1bf84a3bdf564414b8c8bba88e1c869
doom2053 <31608217+doom2053@users.noreply.github.com>
2017-09-03 23:20:18 -0700

    Add files via upload

---
+++ 135 week1/My Project/src/dispenser/Machine.java
@@ -0,0 +1,14 @@
+package dispenser;
+
+public class Machine {
+
+	private String name;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
